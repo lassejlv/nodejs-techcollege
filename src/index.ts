f74cbd0630b36591fb46e5e5251a2a81ccae1bfe
lasse <77295879+lassejlv@users.noreply.github.com>
2024-05-14 10:35:46 +0200

    empty commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import { env } from "./env";
+import { supabase } from "./supabase";
 
 const PORT = env.PORT;
 
@@ -14,10 +15,15 @@ app.get("/", (req, res) => {
   res.send("My express app is running!");
 });
 
-app.get("/another-route", (req, res) => {
-  res.send("Another route!");
-});
+app.get("/songs", async (req, res) => {
+  const songs = await supabase
+    .from("Songs")
+    .select("title, id")
+    .eq("id", "d7423a73-b0e9-4277-8235-bc272547db75")
+    .single();
 
+  res.send(songs);
+});
 app.listen(PORT, () => {
   console.log(`Started server on port http://localhost:${PORT}`);
 });
